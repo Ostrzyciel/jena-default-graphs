029da6a331d39ef7bae9010450b3cfb62863c7cf
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2024-07-14 11:06:36 +0200

    More!

--- jena-default-graphs/src/main/java/org/example/Main.java
+++ jena-default-graphs/src/main/java/org/example/Main.java
@@ -1,14 +1,30 @@
 package org.example;
 
+import org.apache.jena.base.Sys;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.sparql.core.DatasetGraphFactory;
 import org.apache.jena.sparql.core.Quad;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
+
+class Result {
+    public String format;
+    public String caseName;
+    public String status;
+
+    public Result(String format, String caseName, String status) {
+        this.format = format;
+        this.caseName = caseName;
+        this.status = status;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
@@ -36,19 +52,57 @@ public class Main {
                 RDFFormat.TRIG, RDFFormat.JSONLD11, RDFFormat.NQUADS, RDFFormat.RDF_PROTO, RDFFormat.RDF_THRIFT
         );
 
+        var results = new Vector<Result>();
+
         for (var entry : quadsToTest.entrySet()) {
             var quad = entry.getValue();
             var datasetGraph = DatasetGraphFactory.create();
             datasetGraph.add(quad);
 
             for (var format : formats) {
+                var output = new ByteArrayOutputStream();
                 try {
-                    RDFDataMgr.write(OutputStream.nullOutputStream(), datasetGraph, format);
+                    RDFDataMgr.write(output, datasetGraph, format);
                 } catch (Throwable e) {
                     System.out.println("Failed to write " + entry.getKey() + " in " + format);
                     e.printStackTrace();
+                    results.add(new Result(format.toString(), entry.getKey(), "serialization failed"));
+                    continue;
+                }
+
+                if (output.size() == 0) {
+                    System.out.println("Empty output for " + entry.getKey() + " in " + format);
+                    results.add(new Result(format.toString(), entry.getKey(), "empty output"));
+                    continue;
+                }
+
+                var input = new ByteArrayInputStream(output.toByteArray());
+                try {
+                    var readDatasetGraph = DatasetGraphFactory.create();
+                    RDFDataMgr.read(readDatasetGraph, input, format.getLang());
+                    var readQuad = readDatasetGraph.find().next();
+                    if (readQuad.getGraph() == null) {
+                        results.add(new Result(format.toString(), entry.getKey(), "null graph"));
+                    } else if (readQuad.isDefaultGraphExplicit()) {
+                        results.add(new Result(format.toString(), entry.getKey(), "Quad.defaultGraphIRI"));
+                    } else if (readQuad.isDefaultGraphGenerated()) {
+                        results.add(new Result(format.toString(), entry.getKey(), "Quad.defaultGraphNodeGenerated"));
+                    } else {
+                        results.add(new Result(format.toString(), entry.getKey(), "unexpected graph (???)"));
+                    }
+                } catch (Throwable e) {
+                    System.out.println("Failed to read " + entry.getKey() + " in " + format);
+                    e.printStackTrace();
+                    results.add(new Result(format.toString(), entry.getKey(), "deserialization failed"));
                 }
             }
+        }
+
+        System.out.println();
+        System.out.format("%-35s%-25s%-15s%n", "To serialize", "Format", "Status/deserialized");
+
+        for (var result : results) {
+            System.out.format("%-35s%-25s%-15s%n", result.caseName, result.format, result.status);
         }
     }
 }
