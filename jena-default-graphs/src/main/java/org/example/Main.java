bbf98c7d71ef43d88599aa5bb7a8f35c744f2425
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-03-14 15:46:14 +0100

    generalized statements + RIOT = uhhh???

--- jena-default-graphs/src/main/java/org/example/Main.java
+++ jena-default-graphs/src/main/java/org/example/Main.java
@@ -1,26 +1,59 @@
 package org.example;
 
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.sys.JenaSystem;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.RDFParser;
+import org.apache.jena.riot.system.StreamRDFLib;
+import org.apache.jena.riot.system.StreamRDFWriter;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.stream.IntStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var pool = Executors.newFixedThreadPool(8);
+    private static final RDFFormat[] FORMATS = {
+        RDFFormat.NTRIPLES,
+        RDFFormat.TURTLE_BLOCKS,
+        RDFFormat.RDF_PROTO,
+        RDFFormat.RDF_THRIFT,
+    };
 
-        var futures = IntStream.range(0, 16)
-            .mapToObj(i -> pool.submit(() -> {
-                if (i % 2 == 0) ModelFactory.createDefaultModel();
-                else JenaSystem.init();
-                return i;
-            }))
-            .toList();
+    public static void main(String[] args) {
+        var triple = Triple.create(
+            NodeFactory.createLiteralString("subject"),
+            NodeFactory.createLiteralString("predicate"),
+            NodeFactory.createLiteralString("object")
+        );
 
-        for (var future : futures) {
-            System.out.println(future.get());
+        for (var format : FORMATS) {
+            System.out.println();
+            System.out.println();
+            System.out.println(format.toString());
+
+            var os = new ByteArrayOutputStream();
+            var writerOk = false;
+            var readerOk = false;
+            try {
+                var s = StreamRDFWriter.getWriterStream(os, format);
+                s.start();
+                s.triple(triple);
+                s.finish();
+                writerOk = true;
+                try {
+                    var is = new ByteArrayInputStream(os.toByteArray());
+                    var dest = StreamRDFLib.print(System.out);
+                    RDFParser.create().source(is).forceLang(format.getLang()).parse(dest);
+                    readerOk = true;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            System.out.println("Writer: " + (writerOk ? "OK" : "FAIL"));
+            System.out.println("Reader: " + (readerOk ? "OK" : "FAIL"));
         }
     }
 }
